9d5e9b29a5a4ff4d6ed5b789d4d34a18ae75d13c
golyb <golybev2019gg@gmail.com>
2024-01-22 20:51:10 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+import telebot
+from telebot.types import Message
+from config import token
+from jso4k import write_to_file, read_from_db
+
+def update_user_data():
+    return read_from_db()
+
+user_data = update_user_data()
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def start(message:Message):
+    bot.send_message(message.chat.id, "привет")
+    bot.send_message(message.chat.id, "бот рассылает расписание уроков для 7 д класса")
+    user_data[message.chat.id] = message.chat.id
+    write_to_file(user_data)
+
+
+@bot.message_handler(commands=["admin"], func=lambda message:message.from_user.id == 5085094693)
+def admin(message):
+    bot.send_message(message.chat.id, "Ку илюх"
+                                      " Кидай расписание")
+    bot.register_next_step_handler(message, send_raspisane)
+
+def send_raspisane(message):
+    user_data = update_user_data()
+    for chat_id in user_data:
+        bot.send_message(chat_id, message.text)
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(message.chat.id, "Все сломал или нашел не дочёты?")
+    bot.send_message(message.chat.id, 'Свяжись с [создателем](https://t.me/ildics)',
+                     parse_mode='Markdown', )
+
+
+
+
+
+
+
+
+bot.polling()
+
+
---
+++ jso4k.py
@@ -0,0 +1,20 @@
+import json
+
+
+def write_to_file(user_data):
+    json_string = json.dumps(user_data, ensure_ascii=False)
+    print(json_string)
+    with open('db.json', 'w', encoding='utf-8') as file:
+        file.write(json_string)
+    print('готово повелитель!')
+
+def read_from_db():
+    try:
+        with open('db.json', encoding='utf-8') as file:
+            json_string = file.readline()
+        db_data = json.loads(json_string)
+    except FileNotFoundError:
+        db_data = {}
+    print(db_data)
+    return db_data
+
