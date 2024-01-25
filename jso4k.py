225135d11c00aafcd837d48c8f0e608b61081b94
golyb <golybev2019gg@gmail.com>
2024-01-25 19:02:11 +0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- jso4k.py
+++ jso4k.py
@@ -4,13 +4,13 @@ import json
 def write_to_file(user_data):
     json_string = json.dumps(user_data, ensure_ascii=False)
     print(json_string)
-    with open('db.json', 'w', encoding='utf-8') as file:
+    with open('./db.json', 'w', encoding='utf-8') as file:
         file.write(json_string)
-    print('готово повелитель!')
+    print('готово')
 
 def read_from_db():
     try:
-        with open('db.json', encoding='utf-8') as file:
+        with open('./db.json', encoding='utf-8') as file:
             json_string = file.readline()
         db_data = json.loads(json_string)
     except FileNotFoundError:
