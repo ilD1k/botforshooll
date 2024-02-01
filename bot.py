7e04d7c89a3c8588039a6a7d687d6a6e3f2b776f
ilD1k <151182252+ilD1k@users.noreply.github.com>
2024-02-01 18:41:56 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,27 +3,30 @@ from telebot.types import Message
 from config import token
 from jso4k import write_to_file, read_from_db
 
+
 def update_user_data():
     return read_from_db()
+
 
 user_data = update_user_data()
 
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=["start"])
-def start(message:Message):
+def start(message: Message):
     bot.send_message(message.chat.id, "привет")
     bot.send_message(message.chat.id, "бот рассылает расписание уроков для 7 д класса")
     user_data[message.chat.id] = message.chat.id
     write_to_file(user_data)
 
 
-@bot.message_handler(commands=["admin"], func=lambda message:message.from_user.id == 5085094693)
+@bot.message_handler(commands=["admin"], func=lambda message: message.from_user.id == 5085094693)
 def admin(message):
     bot.send_message(message.chat.id, "Ку илюх"
                                       " Кидай расписание")
     bot.register_next_step_handler(message, send_raspisane)
-    
+
 
 def send_raspisane(message):
     user_data = update_user_data()
@@ -33,6 +36,7 @@ def send_raspisane(message):
         except Exception as e:
             print(e, "сломали бота")
 
+
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.send_message(message.chat.id, "Все сломал или нашел не дочёты?")
@@ -40,12 +44,15 @@ def help(message):
                      parse_mode='Markdown', )
     write_to_file(user_data)
 
+@bot.message_handler(content_types=["text"], func=lambda message: message.from_user.id == 236725960)
+def kirill(message):
+    bot.send_message(message.chat.id, "Привет кирюх твое сообщение уже было отправлено Илье")
+    bot.forward_message(5085094693, message.chat.id, message.id)
 
 @bot.message_handler(func=lambda message: True)
 def aga(message):
     bot.send_message(message.chat.id, "И тебе привет")
     write_to_file(user_data)
-
 
 
 
