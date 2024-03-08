e483ac2a92e10936f8eb0c3126008e255746e962
ilD1k <151182252+ilD1k@users.noreply.github.com>
2024-03-08 14:26:53 +0500

    l;eax
    lexa

--- bot.py
+++ bot.py
@@ -5,30 +5,32 @@ from jso4k import write_to_file, read_from_db
 from random import choice
 
 raspisanie = {
-1: f"Расписание на завтра\n 09:00 - 09:45 Ров\n 10:00 - 10:45 История\n 11:00 - 11:45 Русский язык\n "
-   f"12:00 - 12:45 География\n 13:00 - 13:45 Алгебра\n 13:55 - 14:40 Физ-ра\n 14:50 - 15:35 Информатика",
+    1: f"Расписание на завтра\n 09:00 - 09:45 Ров\n 10:00 - 10:45 История\n 11:00 - 11:45 Русский язык\n "
+       f"12:00 - 12:45 География\n 13:00 - 13:45 Алгебра\n 13:55 - 14:40 Физ-ра\n 14:50 - 15:35 Информатика",
 
-2: f"Расписание на завтра\n 08:00 - 08:45 Физика\n 09:00 - 09:45 Обществознание\n 10:00 - 10:45 Литература\n "
-   f"11:00 - 11:45 Русский язык\n 12:00 - 12:45 ОБЖ\n 13:00 - 13:45 Геометрия",
+    2: f"Расписание на завтра\n 08:00 - 08:45 Физика\n 09:00 - 09:45 Обществознание\n 10:00 - 10:45 Литература\n "
+       f"11:00 - 11:45 Русский язык\n 12:00 - 12:45 ОБЖ\n 13:00 - 13:45 Геометрия",
 
-3: f"Расписание на завтра\n 10:00 - 10:45 Алгебра\n 11:00 - 11:45 История\n 12:00 - 12:45 География\n 13:00 - 13:45 Геометрия\n "
-   f"13:55 - 14:40 Иностранный язык",
+    3: f"Расписание на завтра\n 10:00 - 10:45 Алгебра\n 11:00 - 11:45 История\n 12:00 - 12:45 География\n 13:00 - 13:45 Геометрия\n "
+       f"13:55 - 14:40 Иностранный язык",
 
-4: f"Расписание на завтра\n 08:00 - 08:45 Иностранный язык\n 09:00 - 09:45 Русский язык\n 10:00 - 10:45 Литература\n "
-   f"11:00 - 11:45 Технология\n 12:00 - 12:45 Технология\n 13:00 - 13:45 Алгебра\n 14:55 - 14:40 Физика",
+    4: f"Расписание на завтра\n 08:00 - 08:45 Иностранный язык\n 09:00 - 09:45 Русский язык\n 10:00 - 10:45 Литература\n "
+       f"11:00 - 11:45 Технология\n 12:00 - 12:45 Технология\n 13:00 - 13:45 Алгебра\n 14:55 - 14:40 Физика",
 
-5: f"Расписание на завтра\n 08:00 - 08:40 Иностранный язык\n 08:55 - 09:35 Биология\n "
-   f"09:50 - 10:30 Вероятность и статистика\n 10:45 - 11:25 Русский язык\n 11:35 - 12:15 Физ-ра",
+    5: f"Расписание на завтра\n 08:00 - 08:40 Иностранный язык\n 08:55 - 09:35 Биология\n "
+       f"09:50 - 10:30 Вероятность и статистика\n 10:45 - 11:25 Русский язык\n 11:35 - 12:15 Физ-ра",
 
-6: f"Расписание на завтра\n 08:00 - 8:40 Физ-ра\n 08:50 - 9:30 Музыка 09:40 - 10:20 Изо\n 10:30 - 11:10 Русский язык"
+    6: f"Расписание на завтра\n 08:00 - 8:40 Физ-ра\n 08:50 - 9:30 Музыка 09:40 - 10:20 Изо\n 10:30 - 11:10 Русский язык"
 }
 
-
-
-phrases = ["Уффффф чо за гипер стикер пупсик", "Эта типа малеф до травмы в 1999?", "Топ топ ножками в школу рабы системы",
+phrases = ["Уффффф чо за гипер стикер пупсик", "Эта типа малеф до травмы в 1999?",
+           "Топ топ ножками в школу рабы системы",
            "пон", "Поньк", "мегапон", "харош", "сигма", "не будь дурак саси кулак стэтхем"]
 
 phrasesandro = ["Приветствую! Андро", "Здаров!", "Опять сф в мид???", "Че как оно"]
+
+phraseslexa = ["ку леха пузо", "52", "ди ня"]
+
 
 def update_user_data():
     return read_from_db()
@@ -37,7 +39,6 @@ def update_user_data():
 user_data = update_user_data()
 
 bot = telebot.TeleBot(token)
-
 
 
 @bot.message_handler(commands=["start"])
@@ -94,7 +95,7 @@ def day_of_ucheba(message):
     elif message.text == "Чт".lower():
         bot.send_message(message.chat.id, raspisanie[4])
 
-    elif message.text == "Пт".lower():
+    elif message.text == "пт":
         bot.send_message(message.chat.id, raspisanie[5])
 
     elif message.text == "Сб".lower():
@@ -104,9 +105,6 @@ def day_of_ucheba(message):
         bot.register_next_step_handler(message, help)
 
 
-
-
-
 @bot.message_handler(content_types=["text"], func=lambda message: message.from_user.id == 236725960)
 def kirill(message):
     bot.send_message(message.chat.id, "Привет кирюх твое сообщение уже было отправлено Илье")
@@ -114,6 +112,7 @@ def kirill(message):
         bot.send_photo(message.chat.id, phata)
     bot.forward_message(5085094693, message.chat.id, message.id)
     write_to_file(user_data)
+
 
 @bot.message_handler(content_types=["text"], func=lambda message: message.from_user.id == 1305549090)
 def andro(message):
@@ -125,6 +124,12 @@ def andro(message):
     write_to_file(user_data)
 
 
+@bot.message_handler(content_types=["text"], func=lambda message: message.from_user.id == 6408417942)
+def lexa(message):
+    bot.send_message(message.chat.id, choice(phraseslexa))
+    bot.forward_message(5085094693, message.chat.id, message.id)
+    write_to_file(user_data)
+
 
 @bot.message_handler(func=lambda message: True)
 def aga(message):
@@ -132,11 +137,11 @@ def aga(message):
     bot.send_message(message.chat.id, f'И тебе привет! {name}.\n')
     write_to_file(user_data)
 
+
 @bot.message_handler(content_types=["sticker"])
 def sticker(message):
     write_to_file(user_data)
     bot.send_message(message.chat.id, choice(phrases))
-
 
 
 bot.polling()
